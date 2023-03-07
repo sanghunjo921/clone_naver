07ef80a0f837f4a57eff76336b7af76b8a62b90e
Sanghun Jo <salr921@gmail.com>
2023-03-07 14:31:41 +0900

    maincontent layout

--- src/components/ContentCard/ContentCard.js
+++ src/components/ContentCard/ContentCard.js
@@ -1,13 +1,24 @@
 import React from "react";
 import styles from "./contentCard.module.css";
+import DotSVG from "../../shared/svg/dot.svg";
 
 export const ContentCard = ({ image, title, provider, publishedDate }) => {
   return (
     <div className={styles.container}>
       <div className={styles.image}>
-        <img src={image} alt={title}></img>
+        <img className={styles.mainImage} src={image} alt={title}></img>
       </div>
       <div className={styles.title}>{title}</div>
+      <div className={styles.provider}>
+        <img
+          className={styles.providerImg}
+          src={provider.image}
+          alt={provider.name}
+        ></img>
+        <div>{provider.name}</div>
+        <img className={styles.divider} src={DotSVG} alt="dot" />
+        <div className={styles.publishedDate}></div>
+      </div>
     </div>
   );
 };
--- src/components/ContentCard/ContentCard.model.js
+++ src/components/ContentCard/ContentCard.model.js
@@ -5,4 +5,18 @@ export class ContentCardModel {
     this.provider = provider;
     this.publishedDate = publishedDate;
   }
+
+  convertDate() {
+    const today = new Date();
+    return this.calculateDte(today, this.publishedDate);
+  }
+
+  calculateDte(curr, target) {
+    const dateDifference =
+      Math.floor(Math.abs(curr - target)) / (1000 * 60 * 60);
+    if (dateDifference > 24) {
+      dateDifference = dateDifference / 24;
+    }
+    return dateDifference;
+  }
 }
